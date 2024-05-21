91f532c9ce40668a30d466740aab4d2845c0a709
Thienbao6868 <thienbao3018@gmail.com>
2024-05-21 22:27:24 +0700

    spring initial

---
+++ src/test/java/com/Thienbao/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Thienbao.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
