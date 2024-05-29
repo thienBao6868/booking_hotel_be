ba2332eb8f91503c4d1ef37b0966160da85fa1ac
Thienbao6868 <thienbao3018@gmail.com>
2024-05-29 15:43:38 +0700

    Create class CustomAuthenProvider, Thông báo cho AuthenticationManager sử dụng CustomAuthenProvider đó, Authen with data User được lưu trong database

--- src/main/java/com/Thienbao/booking/config/SecurityConfig.java
+++ src/main/java/com/Thienbao/booking/config/SecurityConfig.java
@@ -1,16 +1,32 @@
 package com.Thienbao.booking.config;
 
+import com.Thienbao.booking.security.CustomAuthenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, CustomAuthenProvider customAuthenProvider) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(customAuthenProvider)
+                .build();
+    }
 
 
     @Bean
--- src/main/java/com/Thienbao/booking/controller/AuthController.java
+++ src/main/java/com/Thienbao/booking/controller/AuthController.java
@@ -1,14 +1,30 @@
 package com.Thienbao.booking.controller;
 
+import com.Thienbao.booking.security.CustomAuthenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${api.base-path}/auth")
 public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @GetMapping("/login")
-    public String login(){
-        return "hello";
+    public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password){
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(email,password);
+
+        authenticationManager.authenticate(token);
+
+        return new ResponseEntity<>("Login Success", HttpStatus.OK);
     }
 }
