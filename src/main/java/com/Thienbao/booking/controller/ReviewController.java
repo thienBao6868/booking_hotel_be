386e65e74f62b48de5a0d86da14fe34b8f0e9ee2
Thienbao6868 <thienbao3018@gmail.com>
2024-06-15 00:54:27 +0700

    feature get reviews by hotelier: Create Api, nhận tham số, custom code có thể reUse

--- src/main/java/com/Thienbao/booking/controller/ReviewController.java
+++ src/main/java/com/Thienbao/booking/controller/ReviewController.java
@@ -5,16 +5,14 @@ import com.Thienbao.booking.payload.request.CreateReviewRequest;
 import com.Thienbao.booking.payload.response.BaseResponse;
 import com.Thienbao.booking.security.DataSecurity;
 import com.Thienbao.booking.service.imp.ReviewServiceImp;
+import com.Thienbao.booking.utils.Helper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${api.base-path}/review")
@@ -23,34 +21,27 @@ public class ReviewController {
     @Autowired
     private ReviewServiceImp reviewServiceImp;
 
+    @Autowired
+    private Helper helper;
+
     @PostMapping("")
-    public ResponseEntity<?> createReview(@RequestBody @Valid CreateReviewRequest createReviewRequest){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        DataSecurity dataSecurity = (DataSecurity) authentication.getPrincipal();
-        Long currenUserId = dataSecurity.getId();
-
-
-        BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setStatusCode(HttpStatus.OK.value());
-        baseResponse.setMessage("Create Review successful");
-        baseResponse.setData(reviewServiceImp.createReview(createReviewRequest,currenUserId));
-
-
-      return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+    public ResponseEntity<?> createReview(@RequestBody @Valid CreateReviewRequest createReviewRequest) {
+        Long currenUserId = helper.getCurrentUserId();
+        BaseResponse baseResponse = new BaseResponse(HttpStatus.OK.value(), "Create Review successful", reviewServiceImp.createReview(createReviewRequest, currenUserId), null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.OK);
     };
 
     @PostMapping("/reply")
-    public ResponseEntity<?> createReply(@Valid @RequestBody CreateReplyRequest createReplyRequest){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        DataSecurity dataSecurity = (DataSecurity) authentication.getPrincipal();
-        Long currenUserId = dataSecurity.getId();
-
-        BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setStatusCode(HttpStatus.OK.value());
-        baseResponse.setMessage("Create Reply successful");
-        baseResponse.setData(reviewServiceImp.createReply(createReplyRequest,currenUserId));
-
-      return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+    public ResponseEntity<?> createReply(@Valid @RequestBody CreateReplyRequest createReplyRequest) {
+        Long currenUserId = helper.getCurrentUserId();
+        BaseResponse baseResponse = new BaseResponse(HttpStatus.OK.value(), "Create Reply successful", reviewServiceImp.createReply(createReplyRequest, currenUserId), null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.OK);
     };
 
+    @GetMapping("/hotelier/{hotelId}")
+    public ResponseEntity<?> getReviewsByHotelier(@PathVariable int hotelId) {
+        Long currenUserId = helper.getCurrentUserId();
+        BaseResponse baseResponse = new BaseResponse(HttpStatus.OK.value(), "Get reviews by hotelier successful", currenUserId , null);
+        return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+    };
 }
